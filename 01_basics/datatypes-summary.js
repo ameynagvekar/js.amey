cf71f4c40b6bc618c9f5056e1642f61bf480c2b0
ameynagvekar <78256411+ameynagvekar@users.noreply.github.com>
2024-02-29 16:40:01 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,53 @@
+//premitive
+
+// 7 types : String , number , boolean , null , undefined , Symbol, Bigint
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsidetemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 56468464564646546554n
+console.log(typeof (bigNumber));
+
+//Reference type(non premitive)
+
+//array, objects, functions
+const heros = ["Chaddiman", "Superman", "Batman"]
+
+let obj = {
+    name: "amey",
+    age: 22,
+}
+
+const myfucntion = function () {
+    console.log("Hellow world")
+}
+
+console.log()
+
+// What is typeof?
+
+/*The typeof operator in JavaScript is used to determine the 
+data type of a given value (variable, function, object, etc.).
+It returns a string describing the type.*/
+
+/*Possible Return Values
+
+Numbers: "number"
+Strings: "string"
+Booleans: "boolean"
+Symbols: "symbol"
+Functions: "function"
+Objects: "object"
+Arrays: "object" (Arrays are a special type of object)
+null: "object" (This is a historical peculiarity of JavaScript)
+undefined: "undefined"*/
+
