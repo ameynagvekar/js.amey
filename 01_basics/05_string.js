f78ffb3016c092250362f666db2e66db6e4142dd
ameynagvekar <78256411+ameynagvekar@users.noreply.github.com>
2024-03-07 09:23:10 +0000

    save

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -2,3 +2,35 @@ const name = "Amey"
 const repoCount = 50
 
 console.log(name + repoCount + "value");
+
+console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const gamename = new String('Amey_Pandu')
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+
+
+console.log(gamename.length);
+console.log(gamename.toLocaleUpperCase());
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('A'));
+
+const newString = gamename.substring(0,4);
+console.log(newString)
+
+const anotherString = gamename.slice(-4, 5 );
+
+const newStringone = "   amey  taklu"
+console.log(newStringone)
+console.log(newStringone.trim());
+
+const url = "https:amey.com/amey%20nagvekar"
+
+url.replace('%20', '_');
+console.log(url.replace('%20', '_'));
+
+
+console.log(url.includes('chaman'));
+
+console.log(gamename.split('_'))
